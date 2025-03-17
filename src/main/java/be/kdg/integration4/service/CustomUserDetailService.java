371657a5072686708ff950ffa4816ae71894db43
anir.aouladhaddousaddik <anir.aouladhaddousaddik@student.kdg.be>
2025-03-17 12:47:08 +0100

    Made redirection when role is technician

--- src/main/java/be/kdg/integration4/service/CustomUserDetailService.java
+++ src/main/java/be/kdg/integration4/service/CustomUserDetailService.java
@@ -4,10 +4,13 @@ import be.kdg.integration4.domain.User;
 import be.kdg.integration4.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -21,6 +24,7 @@ public class CustomUserDetailService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(email));
+
         if (!user.isApproved()) {
             throw new DisabledException("Account not approved yet");
         }
