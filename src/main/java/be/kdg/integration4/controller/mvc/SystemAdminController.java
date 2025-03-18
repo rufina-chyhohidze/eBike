0feb60223405d7d17fcf7c7d77b1552ae3a9de32
Rufina Chyhohidze <rufina.chyhohidze@student.kdg.be>
2025-03-18 22:08:58 +0100

    fixed bugs sys-adm

--- src/main/java/be/kdg/integration4/controller/mvc/SystemAdminController.java
+++ src/main/java/be/kdg/integration4/controller/mvc/SystemAdminController.java
@@ -3,9 +3,6 @@ package be.kdg.integration4.controller.mvc;
 import be.kdg.integration4.domain.User;
 import be.kdg.integration4.domain.WorkshopAdmin;
 import be.kdg.integration4.service.UserService;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -54,14 +51,5 @@ public class SystemAdminController {
     public String rejectUser(@PathVariable Long id) {
         userService.rejectUser(id);
         return "redirect:/superadmin/profile";
-    }
-    @PostMapping("/logout")
-    public String logout(HttpServletRequest request, HttpServletResponse response) {
-        try {
-            request.logout();
-        } catch (ServletException e) {
-            e.printStackTrace();
-        }
-        return "redirect:/login?logout";
     }
 }
