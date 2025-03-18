7593061b37d3412cb356765ec0506279a625f17f
Rufina Chyhohidze <rufina.chyhohidze@student.kdg.be>
2025-03-18 12:53:07 +0100

    display user info, logout button, ui.

--- src/main/java/be/kdg/integration4/controller/mvc/SystemAdminController.java
+++ src/main/java/be/kdg/integration4/controller/mvc/SystemAdminController.java
@@ -1,7 +1,11 @@
 package be.kdg.integration4.controller.mvc;
 
 import be.kdg.integration4.domain.User;
+import be.kdg.integration4.domain.WorkshopAdmin;
 import be.kdg.integration4.service.UserService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,22 +13,34 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
 @RequestMapping("/superadmin")
-public class ApprovalController {
+public class SystemAdminController {
+
     private final UserService userService;
 
-    public ApprovalController(UserService userService) {
+    public SystemAdminController(UserService userService) {
         this.userService = userService;
     }
-
-
     @GetMapping("/profile")
-    public String profile(Model model) {
+    public String adminDashboard(Model model, Principal principal) {
+        String email = principal.getName();
+        User user = userService.getUserByEmail(email);
+
+        if (user instanceof WorkshopAdmin) {
+            WorkshopAdmin admin = (WorkshopAdmin) user;
+            model.addAttribute("workshopName", admin.getWorkshop() != null ? admin.getWorkshop().getWorkshopName() : "No Workshop Assigned");
+        } else {
+            model.addAttribute("workshopName", "N/A");
+        }
+
         List<User> pendingUsers = userService.getUnapprovedUsers();
         model.addAttribute("pendingUsers", pendingUsers);
+
+        model.addAttribute("user", user);
         return "super-admin";
     }
 
@@ -39,6 +55,13 @@ public class ApprovalController {
         userService.rejectUser(id);
         return "redirect:/superadmin/profile";
     }
-
-
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            request.logout();
+        } catch (ServletException e) {
+            e.printStackTrace();
+        }
+        return "redirect:/login?logout";
+    }
 }
