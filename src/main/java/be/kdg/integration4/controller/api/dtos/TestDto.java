c6f9e404e1ece9d5b66cd0fc07f8a554bf9f4478
Erik Gerbreders <erik.gerbreders@student.kdg.be>
2025-03-19 18:31:25 +0100

    New TestDto (frontend communication dto) format introduced

--- src/main/java/be/kdg/integration4/controller/api/dtos/TestDto.java
+++ src/main/java/be/kdg/integration4/controller/api/dtos/TestDto.java
@@ -9,21 +9,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TestDto {
-    private int testbenchNumber;
+    private int testBenchNumber;
     private TestType testType;
     private String emailBikeOwner;
     private String chassisNumber;
-    private String bikeBrand;
-    private String bikeSize;
-    private int productionDate;
-    private String type;
-    private String powertrain;
-    private int enginePowerMax;
-    private int mileage;
-    private int accuCapacity;
-    private int enginePowerNominal;
-    private String gearType;
-    private int maxSupport;
-    private int engineTorque;
-    private String engineType;
 }
