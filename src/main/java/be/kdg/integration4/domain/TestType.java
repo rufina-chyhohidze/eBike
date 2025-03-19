5ea5afafcbbed2375edeffad9f814355df18d227
Erik Gerbreders <erik.gerbreders@student.kdg.be>
2025-03-19 21:09:43 +0100

    Quick merge issue fix

--- src/main/java/be/kdg/integration4/domain/TestType.java
+++ src/main/java/be/kdg/integration4/domain/TestType.java
@@ -1,6 +1,7 @@
 package be.kdg.integration4.domain;
 
 public enum TestType {
+    FAST,
     STRESS,
     EFFICIENCY,
     MANUAL
--- src/main/resources/static/js/process-test.js
+++ src/main/resources/static/js/process-test.js
@@ -1,8 +1,8 @@
-const DOMAIN_NAME = window.location.hostname + (window.location.port ? `:${window.location.port}` : '');
-
 import { customerFound, showCustomerBikes } from "./start-test.js";
 
-const DOMAIN_NAME = "localhost:8080"
+const DOMAIN_NAME = window.location.hostname + (window.location.port ? `:${window.location.port}` : '');
+
+
 
 
 const form = document.querySelector("form");
