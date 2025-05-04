22614d85f87ff89521c334ee77e181a7c82c736f
Anir Saddik <anirsaddik@localhost.localdomain>
2025-05-04 19:33:18 +0200

    fixed variables issue in cicd

--- azure-config/login.sh
+++ azure-config/login.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-# Path to Azure credentials JSON file (GitLab CI mounts it as a temp file)
-CREDENTIALS_FILE="$AZURE_LOGIN_CREDENTIALS"
+# Variables with values needed to login to azure account which are stored (masked and hidden) in gitlab ci/cd variables
+USERNAME=$CLIENT_ID
+PASSWORD=$CLIENT_SECRET
+TENANT=$TENANT_ID
 
 # Perform Azure login using service principal
 az login --service-principal \
-  --username "$(jq -r .clientId "$CREDENTIALS_FILE")" \
-  --password "$(jq -r .clientSecret "$CREDENTIALS_FILE")" \
-  --tenant "$(jq -r .tenantId "$CREDENTIALS_FILE")"
+  --username "$USERNAME" \
+  --password "$PASSWORD" \
+  --tenant "$TENANT"
 
 # Confirm login
-az account show && echo "✅ Azure login successful" && exit 0
+az account show && echo "✅ Azure login successful"# && exit 0
 
-echo "Error logging in" ; exit 1 ;
+#echo "Error logging in" ; exit 1 ;
+
+# Confirm login and check if it was successful
+if az account show; then
+  echo "✅ Azure login successful"
+  exit 0
+else
+  echo "❌ Error logging in to Azure"
+  exit 1
+fi
