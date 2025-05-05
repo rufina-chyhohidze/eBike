677cf85260108eb7a045447861676e696a0f4390
anir.aouladhaddousaddik <anir.aouladhaddousaddik@student.kdg.be>
2025-05-05 04:08:52 +0200

    vm synch

--- azure-config/create-virtual-machine.sh
+++ azure-config/create-virtual-machine.sh
@@ -5,38 +5,45 @@ echo "Creating the resource group..."
 az group create --name rg-team18-integration4 --location westeurope
 echo "Resource group created"
 
-
+echo "Setting up ssh keys"
 mkdir -p ~/.ssh
-#ssh-keygen -t ed25519 -f ~/.ssh/azure -C "team18" -N "" # Generate key with no passphrase
 echo "$PRIVATE_KEY" | base64 -d > ~/.ssh/azure
 echo "$PUBLIC_KEY" > ~/.ssh/azure.pub
+echo "Finished setting up ssh keys"
 
-
-# Save the public key for attaching to Azure VM
-SSH_PUBLIC_KEY=$(cat ~/.ssh/azure.pub)
-
-# Optionally, echo the public key for debugging purposes
-echo "Generated SSH Public Key: $SSH_PUBLIC_KEY"
-
+echo "Create Virtual Machine"
 az vm create --name vm-team18-integration4 \
 --resource-group rg-team18-integration4 \
 --size Standard_B2s \
 --accept-term \
 --image 'almalinux:almalinux-x86_64:9-gen1:latest' \
 --admin-username team18 \
---zone 2 \
 --public-ip-sku Standard \
---public-ip-dns-name "team18-integration4" \
+--public-ip-address pip-vm-team18-integration4 \
 --storage-sku Standard_LRS \
 --os-disk-name osdisk-vm-team18-integration4 \
 --accelerated-networking false \
 --data-disk-delete-option delete \
 --os-disk-delete-option delete \
---ssh-key-value "$SSH_PUBLIC_KEY" \
+--ssh-key-value ~/.ssh/azure.pub \
 --nsg nsg-team18-integration4 \
 --vnet-name vnet-team18-integration4 \
 --tags 'ContactEmail=team18integration4@gmail.com'
+echo "Virtual Machine Created"
 
+VM_IP=$(az vm show --name vm-team18-integration4 --resource-group rg-team18-integration4 --query "publicIpAddress" -o tsv)
+echo "IP address of vm created: $VM_IP"
+
+echo "Uploading app to the VM..."
+scp -i ~/.ssh/azure -r "$CI_PROJECT_DIR/build/libs/" "team18@$VM_IP":/home/team18/libs
+
+
+ssh -i ~/.ssh/azure "team18@$VM_IP" << 'EOF'
+    echo "hey from vm"
+    cd /home/team18/libs
+    ls
+EOF
+echo "exited vm"
 
 #read -r -p "Do you want to destroy all resources? (yes to proceed, no to cancel): " response
 #
