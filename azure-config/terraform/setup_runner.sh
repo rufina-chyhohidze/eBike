70b17f919390b647e3e0a253a6908957731c52bb
anir.aouladhaddousaddik <anir.aouladhaddousaddik@student.kdg.be>
2025-05-16 19:33:10 +0200

    ssh in root

--- azure-config/terraform/setup_runner.sh
+++ azure-config/terraform/setup_runner.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Exits script immediately if a command fails
-#set -e
-
 echo "Getting VM public ip address..."
 VM_IP="$(az vm list-ip-addresses --resource-group rg-team18 --name vm-team18 --query "[].virtualMachine.network.publicIpAddresses[].ipAddress" -o tsv)"
 echo "VM public ip address: $VM_IP"
@@ -11,9 +8,6 @@ echo "VM public ip address: $VM_IP"
 echo "Adding VM IP to known hosts..."
 ssh-keyscan -H "$VM_IP" >> ~/.ssh/known_hosts
 echo "VM IP added to known hosts"
-
-
-# sudo dnf upgrade -y
 
 echo "Initiating connection with VM to setup runner..."
 ssh -i ~/.ssh/azure team18@"$VM_IP" << 'SetupInput'
