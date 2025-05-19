a22d380b0076e9549c0ce4585a4a9bc7a38068d1
Erik Gerbreders <erik.gerbreders@student.kdg.be>
2025-05-19 11:51:01 +0200

    Removed redundant code

--- src/main/resources/static/js/report-comparison.js
+++ src/main/resources/static/js/report-comparison.js
@@ -33,26 +33,3 @@ compareButton.addEventListener("click", async () => {
         })
     }
 })
-
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     const compareSelect = document.getElementById('compareId');
-//     const compareButton = document.getElementById('compareButton');
-//async
-//     if (compareSelect) {
-//         compareSelect.addEventListener('change', function () {
-//             const selectedId = this.value;
-//             const currentUrl = new URL(window.location.href);
-//
-//             if (selectedId) {
-//                 currentUrl.searchParams.set('compareId', selectedId);
-//                 compareButton.disabled = false;
-//             } else {
-//                 currentUrl.searchParams.delete('compareId');
-//                 compareButton.disabled = true;
-//             }
-//
-//             window.location.href = currentUrl.toString();
-//         });
-//     }
-// });
