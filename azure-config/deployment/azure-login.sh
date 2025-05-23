1dcf1fc784713a1b2a2871dfb3c8834149fa404c
anir.aouladhaddousaddik <anir.aouladhaddousaddik@student.kdg.be>
2025-05-23 17:06:27 +0200

    deploy with https

--- azure-config/deployment/azure-login.sh
+++ azure-config/deployment/azure-login.sh
@@ -6,3 +6,22 @@ az login --service-principal \
 --tenant ed1fc57f-8a97-47e7-9de1-9302dfd786ae
 
 az account set -s "$(az account show --query id --output tsv)"
+
+
+#Command to get ssl certificates:
+
+# First build certduck image with Dockerfile in nginx directory:
+docker build -t certduck {dir of dockerfile}
+
+#docker run \
+#-v "./letsencrypt:/etc/letsencrypt" \
+#-v "./log/letsencrypt:/var/log/letsencrypt" \
+#certduck \
+#certonly \
+#--non-interactive \
+#--agree-tos \
+#--email team18@student.kdg.be \
+#--preferred-challenges dns \
+#--authenticator dns-duckdns \
+#--dns-duckdns-token "b4bb4460-f9d0-42fc-a063-e1dbadd11014" \
+#-d "team18-int4.duckdns.org"
