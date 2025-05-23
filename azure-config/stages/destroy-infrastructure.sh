1692dd6e8a91f98f3a788e80b6ff67b9bfb9e1ee
anir.aouladhaddousaddik <anir.aouladhaddousaddik@student.kdg.be>
2025-05-23 20:41:18 +0200

    Destroy Stages

---
+++ azure-config/stages/destroy-infrastructure.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function destroyAll() {
+  for rg in $(az group list --query "[].name" -o tsv); do
+    az group delete --name "$rg" --yes --no-wait
+  done
+}
+
+function destroyRg() {
+  az group delete --name "$1" --yes --no-wait
+}
+
+if [[ "${DESTROY_ALL:-false}" == "true" ]]; then
+  destroyAll
+elif [[ "${DESTROY_DEPLOY:-false}" == "true" ]]; then
+  destroyRg "rg-team18-deploy"
+elif [[ "${DESTROY_RUNNER:-false}" == "true" ]]; then
+  destroy_infra "rg-team18"
+else
+  echo "❌ Error: No DESTROY_* variable set."
+  exit 1
+fi
